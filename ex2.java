74fe751f97b346137369b04e1ecbe00c9285a415
ciaray <48371206+ciaray@users.noreply.github.com>
2019-03-09 13:46:49 +0000

    Add files via upload

---
+++ ex2.java
@@ -0,0 +1,19 @@
+package exercises;
+
+// program to get character at given index in a string
+
+public class ex2 {
+
+	public static void main(String[] args) {
+
+		// Define original string
+		String originalString = "Java Exercises!";
+		
+		//Print out characters at indexes 0 and 10
+		System.out.println("The character at position 0 is " + originalString.charAt(0));
+		System.out.println("The character at position 10 is " + originalString.charAt(10));
+		
+		
+	}
+
+}
